124f219419ea4ffcedf0e2cc9feae48c1cf8c5e8
aivaz88 <aivaz1988@mail.ru>
2022-10-18 00:12:18 +0500

    Sum all numbers

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+SumAllNumbers(M, N);
+
+
+void SumAllNumbers(int M, int N, int sum = 0)
+{
+    if (N == M) Console.Write(sum += M);
+    else 
+    {
+        sum += M;
+        SumAllNumbers(++M, N, sum);
+    }
+}
